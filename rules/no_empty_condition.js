bc740698f5f3ddd334975ea9e3641c535e5ac3cb
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2015-07-22 18:00:37 +0200

    Add no_empty_condition rule, closes #8

---
+++ rules/no_empty_condition.js
@@ -0,0 +1,50 @@
+var C = require('../lib/constants');
+var problem = require('../lib/problem');
+
+var RULE_NAME = 'no_empty_condition';
+var WARNING_MESSAGE = 'Leaving empty condition blocks is discouraged.';
+
+module.exports = {
+    run: function(node, done) {
+        if (node.type === 'Condition' && (Array.isArray(node.otherwise) || node.conditions.length > 1)) {
+            if (Array.isArray(node.otherwise) && hasEmptyContent(node.otherwise)) {
+                return done(
+                    problem(
+                        RULE_NAME,
+                        C.RESULT_TYPES.WARNING,
+                        WARNING_MESSAGE,
+                        node.position
+                    )
+                );
+            }
+
+            for (var i = 0; i < node.conditions.length; i += 1) {
+                if (hasEmptyContent(node.conditions[i].content)) {
+                    return done(
+                        problem(
+                            RULE_NAME,
+                            C.RESULT_TYPES.WARNING,
+                            WARNING_MESSAGE,
+                            node.position
+                        )
+                    );
+                }
+            }
+        }
+
+        return done(
+            problem(RULE_NAME, C.RESULT_TYPES.NO_PROBLEMS)
+        );
+    }
+};
+
+function hasEmptyContent(content) {
+    return content
+        .filter(function(node) {
+            return !(
+                node.type === 'Text' &&
+                node.content.match(/^\s+$/g)
+            );
+        })
+        .length === 0;
+}
