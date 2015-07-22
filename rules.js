054af5d36b8779ffe91bc4af765b83da224899b3
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2015-07-22 15:57:50 +0200

    Add no_generate_variant rule, closes #9

--- rules.js
+++ rules.js
@@ -1,6 +1,7 @@
 module.exports = [
     require('./rules/did_you_mean'),
     require('./rules/no_complex_unless'),
+    require('./rules/no_generate_variant'),
     require('./rules/no_invalid_tags'),
     require('./rules/no_tmpl_loop')
 ];
---
+++ rules/no_generate_variant.js
@@ -0,0 +1,42 @@
+var C = require('../lib/constants');
+var problem = require('../lib/problem');
+
+var RULE_NAME = 'no_generate_variant';
+var GENERATE_VARIANT_VARIABLE_PREFIX = 'b_generate_variant_for_js_tracking';
+
+module.exports = {
+    run: function(node, done) {
+        var hasError = (
+            (
+                node.type === 'SingleAttribute' &&
+                mentionsGenerateVariant(node.name)
+            ) || (
+                node.type === 'PairAttribute' &&
+                typeof node.value === 'string' &&
+                mentionsGenerateVariant(node.value)
+            ) || (
+                node.type === 'Expression' &&
+                mentionsGenerateVariant(node.value)
+            )
+        );
+
+        if (hasError) {
+            return done(
+                problem(
+                    RULE_NAME,
+                    C.RESULT_TYPES.ERROR,
+                    'b_generate_variant_for_js_tracking is deprecated.',
+                    node.position
+                )
+            );
+        }
+
+        return done(
+            problem(RULE_NAME, C.RESULT_TYPES.NO_PROBLEMS)
+        );
+    }
+};
+
+function mentionsGenerateVariant(string) {
+    return string.indexOf(GENERATE_VARIANT_VARIABLE_PREFIX) !== -1
+}
