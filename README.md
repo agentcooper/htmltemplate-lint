9977ab148fd0b691d310a209807f7dca12b61b7f
Artem Tyurin <artem.tyurin@gmail.com>
2015-07-22 23:02:21 +0200

    SublimeLinter plugin

--- README.md
+++ README.md
@@ -6,6 +6,26 @@
 node index.js ./test.inc
 ```
 
+#### Linter
+
+Linter is based on SublimeLinter, so install that first.
+
+```
+cd /Users/{USERNAME}/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+
+ln -s ~/htmltemplate-lint/SublimeLinter-contrib-htmltemplate-lint SublimeLinter-contrib-htmltemplate-lint
+```
+
+Go to `Sublime Text > Preferences > Package Settings > SublimeLinter > Settings - User` and to 'linters'
+
+```json
+"htmltemplatelint": {
+    "@disable": false,
+    "args": [],
+    "excludes": []
+}
+```
+
 #### Tests
 ```
 mocha
---
+++ SublimeLinter-contrib-htmltemplate-lint/linter.py
@@ -0,0 +1,40 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Artem Tyurin
+# Copyright (c) 2015 Artem Tyurin
+#
+# License: MIT
+#
+
+"""This module exports the HtmltemplateLint plugin class."""
+
+from SublimeLinter.lint import NodeLinter, util
+
+
+class HtmltemplateLint(NodeLinter):
+
+    """Provides an interface to htmltemplate-lint."""
+
+    syntax = 'html'
+    cmd = ('htmltemplate-lint', '--lint', '@')
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0'
+    regex = (
+        r'^.+?: line (?P<line>\d+), col (?P<col>\d+), '
+        r'(?:(?P<error>Error)|(?P<warning>Warning)) - '
+        r'(?P<message>.+)'
+    )
+    multiline = False
+    line_col_base = (1, 1)
+    tempfile_suffix = None
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
+    inline_settings = None
+    inline_overrides = None
+    comment_re = r'\s*/[/*]'
+
