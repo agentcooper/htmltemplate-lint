124fcc11e292183208e4bfa0fbf98b83056550ed
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2015-07-22 16:27:50 +0200

    Use commander for CLI, closes #16

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-require('./cli')(process.argv);
+var fs = require('fs');
+var inspect = require('util').inspect;
+
+var parser = require('htmltemplate-parser');
+
+var run = require('./run');
+var rules = require('./rules');
+
+module.exports = function(filename) {
+    var tmpl = fs.readFileSync(filename, 'utf8');
+
+    try {
+        var ast = parser.parse(tmpl);
+    } catch(e) {
+        console.error(e);
+    }
+
+    run(ast, rules, function(log) {
+        console.log(inspect(log, { colors: true, depth: Infinity }));
+    });
+};
