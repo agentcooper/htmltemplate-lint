9b3eeeaca091291dcdcc84c6a7342229eef8c377
Artem Tyurin <artem.tyurin@gmail.com>
2015-07-28 13:53:19 +0200

    did_you_mean: use config

--- booking-config.js
+++ booking-config.js
@@ -8,5 +8,18 @@ module.exports = {
             return name.indexOf('b_') !== 0;
         },
         message: 'Variables in templates should start with b_.'
+    },
+
+    did_you_mean: {
+        mistyped: [
+            {
+                search: 'b_experiment_hash',
+                message: 'Did you mean b_experiment_hash?'
+            },
+            {
+                search: 'b_track_experiment',
+                message: 'Did you mean b_track_experiment?'
+            }
+        ]
     }
 };
--- rules/did_you_mean.js
+++ rules/did_you_mean.js
@@ -7,54 +7,49 @@ var RULE_NAME = 'did_you_mean';
 
 var PERL_IDENTIFIER = /\$[_0-9a-zA-Z]+/g;
 
-var mistyped = [
-    {
-        search: 'b_experiment_hash',
-        message: 'Did you mean b_experiment_hash?'
-    },
-    {
-        search: 'b_track_experiment',
-        message: 'Did you mean b_track_experiment?'
-    }
-];
+function setup(config) {
+    var mistyped = config.mistyped;
 
-module.exports = {
-    run: function(node, done) {
-        var content = getContent(node);
+    return {
+        run: function(node, done) {
+            var content = getContent(node);
 
-        if (content.length === 0) {
-            return;
-        }
+            if (content.length === 0) {
+                return;
+            }
 
-        for (var j = 0; j < content.length; j++) {
-            for (var i = 0; i < mistyped.length; i++) {
-                var distance = (
-                    new Levenshtein(
-                        // @TODO: is there another way, without slicing?
-                        content[j].slice(0, mistyped[i].search.length),
-                        mistyped[i].search
-                    )
-                ).distance;
-
-                // @FIXME: find a good range
-                if (distance > 0 && distance < 5) {
-                    return done(
-                        problem(
-                            RULE_NAME,
-                            C.RESULT_TYPES.WARNING,
-                            mistyped[i].message,
-                            node
+            for (var j = 0; j < content.length; j++) {
+                for (var i = 0; i < mistyped.length; i++) {
+                    var distance = (
+                        new Levenshtein(
+                            // @TODO: is there another way, without slicing?
+                            content[j].slice(0, mistyped[i].search.length),
+                            mistyped[i].search
                         )
-                    );
+                    ).distance;
+
+                    // @FIXME: find a good range
+                    if (distance > 0 && distance < 5) {
+                        return done(
+                            problem(
+                                RULE_NAME,
+                                C.RESULT_TYPES.WARNING,
+                                mistyped[i].message,
+                                node
+                            )
+                        );
+                    }
                 }
             }
-        }
 
-        return done(
-            problem(RULE_NAME, C.RESULT_TYPES.NO_PROBLEMS)
-        );
+            return done(
+                problem(RULE_NAME, C.RESULT_TYPES.NO_PROBLEMS)
+            );
+        }
     }
 };
+
+module.exports = setup;
 
 function getContent(node) {
     if (node.type === 'SingleAttribute') {
